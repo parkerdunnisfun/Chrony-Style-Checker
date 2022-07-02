3dc232d946c4ca47eb0817e516cc55bd7c25bb49
parkerdunnisfun <dunn.pjd@gmail.com>
2022-07-02 11:38:44 -0600

    fixed bug confusing Dec. with Feb., wrote tests

--- Program.py
+++ Program.py
@@ -17,56 +17,59 @@ months = {
     }
 
 def isValidDate(date):    
-    #does date follow requested format?
+    # does date follow requested format?
     if len(date) == 10:
         for idx, x in enumerate(date):
             if idx != 2 and idx != 5:
                 if not isdigit(x):
-                    return False;
+                    return False
             elif x != '/':
-                return False;
-    else: return False;
+                return False
+    else: return False
 
-    #dose date respect no-zero rule?
+    # dose date respect no-zero rule?
     if int(date[0:2]) == 0 or int(date[3:5]) == 0 or int(date[6:]) == 0:
-        return False;
+        return False
 
-    #does date respect maximum months rule?
+    # does date respect maximum months rule?
     if int(date[0:2]) > 12:
-        return False;
+        return False
 
-    #does date respect maximum days rule?
+    # does date respect maximum days rule?
     if int(date[1]) % 2 == 0:
         #if month is february
-        if int(date[1]) == 2:
+        if int(date[1]) == 2 and int(date[0]) == 0:
             if int(date[3:5]) > 28:
                 if not isleap(int(date[6:10])):
-                    return False;
+                    return False
                 elif int(date[3:5]) > 29:
-                    return False;
-        #month is not february
+                    return False
+        # month is not february
         elif int(date[0:2]) < 8:
             if int(date[3:5]) > 30:
-                return False;
+                return False
         elif int(date[3:5]) > 31:
-            return False;
+            return False
     elif int(date[0:2]) >= 8:
         if int(date[3:5]) > 30:
-            return False;
+            return False
     elif int(date[3:5]) > 31:
-        return False;
+        return False
 
-    return True;
+    return True
 
 def convert(date):
     
     if not isValidDate(date):
         return "Error: invalid date"
 
-    dateArray = date.split('/');
+    dateArray = date.split('/')
 
-    return months[dateArray[0]] + " " + dateArray[1].lstrip('0') + ", " + dateArray[2].lstrip('0');
+    return months[dateArray[0]] + " " + dateArray[1].lstrip('0') + ", " + dateArray[2].lstrip('0')
 
-date = input("Type a date in mm/dd/yyyy format: ");
+def main():
+    date = input("Type a date in mm/dd/yyyy format: ")
+    print(convert(date))
 
-print(convert(date));
+if __name__ == "__main__":
+    main()
